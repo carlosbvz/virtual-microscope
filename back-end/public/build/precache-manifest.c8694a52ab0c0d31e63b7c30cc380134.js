5c32630ed48ba1fcfa3fcae8896848c804295ea9
Carlos Benavides <carlosbvz@gmail.com>
2020-09-26 11:25:14 -0600

    Adding build folder to use Heroku

---
+++ back-end/public/build/precache-manifest.c8694a52ab0c0d31e63b7c30cc380134.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d2ce4df56402f56f7e7c43cba500554",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38166ef1c30553973208",
+    "url": "/static/css/5.a4567992.chunk.css"
+  },
+  {
+    "revision": "5701da3ebe4f6280a092",
+    "url": "/static/css/main.8a3b3eb1.chunk.css"
+  },
+  {
+    "revision": "a0404322ab6ab9421887",
+    "url": "/static/js/0.95267ea2.chunk.js"
+  },
+  {
+    "revision": "b53fee4c03b4db851d18",
+    "url": "/static/js/3.5df15ec3.chunk.js"
+  },
+  {
+    "revision": "ef90dec32da5a94b550496958cb808df",
+    "url": "/static/js/3.5df15ec3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "534564a7c97edf2fe585",
+    "url": "/static/js/4.a98e7ac8.chunk.js"
+  },
+  {
+    "revision": "fd7525d544dd9c67d07855cb8778e590",
+    "url": "/static/js/4.a98e7ac8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38166ef1c30553973208",
+    "url": "/static/js/5.e2f4d9bb.chunk.js"
+  },
+  {
+    "revision": "223e1043e6c92cd5b8490b603521d509",
+    "url": "/static/js/5.e2f4d9bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c596cca4bc9b68cecc35",
+    "url": "/static/js/6.332cbc8d.chunk.js"
+  },
+  {
+    "revision": "48bcb6ef47281a292174",
+    "url": "/static/js/7.3bd9ce18.chunk.js"
+  },
+  {
+    "revision": "75171b825929a1916fb3",
+    "url": "/static/js/8.97db418d.chunk.js"
+  },
+  {
+    "revision": "ec22895465535bacb221",
+    "url": "/static/js/9.d8ed1631.chunk.js"
+  },
+  {
+    "revision": "5701da3ebe4f6280a092",
+    "url": "/static/js/main.d64abb48.chunk.js"
+  },
+  {
+    "revision": "0080ef7ae07715d40d27",
+    "url": "/static/js/runtime-main.df311c63.js"
+  }
+]);
