6874a4dd4c41f5c0d20f2c5ec74aa7f1add82a57
Carlos Benavides <carlosbvz@gmail.com>
2020-09-26 10:06:39 -0600

    Testing Heroku with build folder

---
+++ back-end/public/build/precache-manifest.603d1ef88ae6c6a59dd4716735474af8.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0593919c0bfda57d748755116217289a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38166ef1c30553973208",
+    "url": "/static/css/5.a4567992.chunk.css"
+  },
+  {
+    "revision": "5701da3ebe4f6280a092",
+    "url": "/static/css/main.8a3b3eb1.chunk.css"
+  },
+  {
+    "revision": "a0404322ab6ab9421887",
+    "url": "/static/js/0.95267ea2.chunk.js"
+  },
+  {
+    "revision": "b53fee4c03b4db851d18",
+    "url": "/static/js/3.5df15ec3.chunk.js"
+  },
+  {
+    "revision": "ef90dec32da5a94b550496958cb808df",
+    "url": "/static/js/3.5df15ec3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "534564a7c97edf2fe585",
+    "url": "/static/js/4.a98e7ac8.chunk.js"
+  },
+  {
+    "revision": "fd7525d544dd9c67d07855cb8778e590",
+    "url": "/static/js/4.a98e7ac8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38166ef1c30553973208",
+    "url": "/static/js/5.e2f4d9bb.chunk.js"
+  },
+  {
+    "revision": "223e1043e6c92cd5b8490b603521d509",
+    "url": "/static/js/5.e2f4d9bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c596cca4bc9b68cecc35",
+    "url": "/static/js/6.332cbc8d.chunk.js"
+  },
+  {
+    "revision": "48bcb6ef47281a292174",
+    "url": "/static/js/7.3bd9ce18.chunk.js"
+  },
+  {
+    "revision": "414b64c9e8a40db8567e",
+    "url": "/static/js/8.d11e2c47.chunk.js"
+  },
+  {
+    "revision": "ec22895465535bacb221",
+    "url": "/static/js/9.d8ed1631.chunk.js"
+  },
+  {
+    "revision": "5701da3ebe4f6280a092",
+    "url": "/static/js/main.d64abb48.chunk.js"
+  },
+  {
+    "revision": "b03d869ac6339c49a614",
+    "url": "/static/js/runtime-main.55751737.js"
+  }
+]);
